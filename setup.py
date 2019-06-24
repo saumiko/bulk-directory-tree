992f1ed2273ab865a1179de81196784e1aafb9b1
Asif Mohaimen <asifmohaimen@student.sust.edu>
2019-06-24 11:14:21 +0600

    Python3 support added

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bulk_directory_tree',
-    version='0.0.6',
+    version='0.0.7',
     py_modules=['bulk_directory_tree.py'],
     install_requires=[
         'Click==7.0'
     ],
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
     entry_points='''
         [console_scripts]
         bulk-directory-tree=bulk_directory_tree:main
@@ -20,4 +21,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/saumiko/bulk-directory-tree",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
